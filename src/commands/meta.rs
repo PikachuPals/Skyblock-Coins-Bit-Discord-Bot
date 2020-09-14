6570a39cd61b3eddc54e2e48e3b444669da58a17
Andy Lau <pikachupalsmail@gmail.com>
2020-09-14 01:57:10 +0100

    Added coins per bits functionality

--- src/commands/meta.rs
+++ src/commands/meta.rs
@@ -8,22 +8,22 @@ use serenity::framework::standard::{
 use requests::ToJson;
 
 use std::collections::HashMap;
+use std::env;
+use std::fs::File;
 
 #[command]
 fn stats(ctx: &mut Context, msg: &Message) -> CommandResult {
 
-    let _ = msg.channel_id.say(&ctx.http, "Working...");
-    let mut bits_item_map = HashMap::new();
+    let hypixel_token = env::var("HYPIXEL_TOKEN")
+        .expect("Expected hypixel token in the environment");
 
-    bits_item_map.insert(BitsItemPrices::new("God Potion", 2000), 1);
-    bits_item_map.insert(BitsItemPrices::new("Kat Flower", 500), 1);
-    bits_item_map.insert(BitsItemPrices::new("Heat Core", 3000), 1);
-    bits_item_map.insert(BitsItemPrices::new("Hyper Catalyst Upgrade", 300), 1);
-    bits_item_map.insert(BitsItemPrices::new("Ultimate Carrot Candy Upgrade", 8000), 1);
-    bits_item_map.insert(BitsItemPrices::new("Colossal Experience Bottle Upgrade", 1200), 1);
-    bits_item_map.insert(BitsItemPrices::new("Jumbo Backpack Upgrade", 4000), 1);
-    bits_item_map.insert(BitsItemPrices::new("Minion Storage X-pender", 1500), 1);
-    bits_item_map.insert(BitsItemPrices::new("Hologram", 2000), 1);
+    let _ = msg.channel_id.say(&ctx.http, "Working...");
+
+    let skyblock_bazaar_cookie = format!("https://api.hypixel.net/skyblock/bazaar?key={}", hypixel_token);
+    let response = requests::get(skyblock_bazaar_cookie).unwrap();
+    let data = response.json().unwrap();
+
+    let buy_cookie_price = &data["products"]["BOOSTER_COOKIE"]["sell_summary"][0]["pricePerUnit"];
 
 //    let mojang_response = requests::get("https://api.mojang.com/users/profiles/minecraft/PikachuPals").unwrap();
 //    let mojang_data = mojang_response.json().unwrap();
@@ -35,7 +35,7 @@ fn stats(ctx: &mut Context, msg: &Message) -> CommandResult {
 //    let response = requests::get(skyblock_request).unwrap();
 //    let data = response.json().unwrap();
 
-    let skyblock_auctions = String::from(" https://api.hypixel.net/skyblock/auctions?page=0");
+    let skyblock_auctions = String::from("https://api.hypixel.net/skyblock/auctions?page=0");
     let response = requests::get(skyblock_auctions).unwrap();
     let data = response.json().unwrap();
 
@@ -43,8 +43,20 @@ fn stats(ctx: &mut Context, msg: &Message) -> CommandResult {
 
     let bits_items_lowest_prices = get_lowest_bin_values(auction_pages);
 
+    let bits_item_cost_vec = vec![2000, 500, 3000, 300, 8000, 1200, 4000, 1500, 2000];
+    let item_array: [String; 9] = ["God Potion".to_string(), "Kat Flower".to_string(), "Heat Core".to_string(), "Hyper Catalyst Upgrade".to_string(), "Ultimate Carrot Candy Upgrade".to_string(),
+    "Colossal Experience Bottle Upgrade".to_string(), "Jumbo Backpack Upgrade".to_string(), "Minion Storage X-pender".to_string(), "Hologram".to_string()];
+
+    let mut bits_item_vec = vec![];
+
     for (item, price) in bits_items_lowest_prices.iter(){
-        println!("{}'s lowest price is {}", item, price);
+        let index = item_array.iter().position(|x| x == item).unwrap();
+        bits_item_vec.push(BitsItemPrices::new(item, bits_item_cost_vec[index], *price));
+    }
+
+    for listing in bits_item_vec{
+        let output_message = format!("{} costs {} bits. Lowest BIN is {} where coins/bit is at {}.", listing.bits_item, listing.bits_cost, listing.lowest_cost, listing.coins_per_bit());
+        let _ = msg.channel_id.say(&ctx.http, output_message);
     }
 
     let _ = msg.channel_id.say(&ctx.http, "Finished");
@@ -56,11 +68,16 @@ fn stats(ctx: &mut Context, msg: &Message) -> CommandResult {
 struct BitsItemPrices{
     bits_item: String,
     bits_cost: i32,
+    lowest_cost: i32,
 }
 
 impl BitsItemPrices{
-    fn new(bits_item: &str, bits_cost: i32) -> BitsItemPrices{
-        BitsItemPrices {bits_item: bits_item.to_string(), bits_cost: bits_cost}
+    fn new(bits_item: &str, bits_cost: i32, lowest_cost: i32) -> BitsItemPrices{
+        BitsItemPrices {bits_item: bits_item.to_string(), bits_cost: bits_cost, lowest_cost: lowest_cost}
+    }
+
+    fn coins_per_bit(&self) -> i32 {
+        (self.lowest_cost / self.bits_cost).abs()
     }
 }
 
