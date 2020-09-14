778cb382b3f9981334a29d88b629435f994b275e
Andy Lau <pikachupalsmail@gmail.com>
2020-09-14 01:56:48 +0100

    Testing

--- src/commands/math.rs
+++ src/commands/math.rs
@@ -5,6 +5,10 @@ use serenity::framework::standard::{
     macros::command,
 };
 
+use requests::ToJson;
+
+use std::env;
+
 #[command]
 pub fn multiply(ctx: &mut Context, msg: &Message, mut args: Args) -> CommandResult {
     let one = args.single::<f64>().unwrap();
@@ -13,6 +17,23 @@ pub fn multiply(ctx: &mut Context, msg: &Message, mut args: Args) -> CommandResu
     let product = one * two;
 
     let _ = msg.channel_id.say(&ctx.http, product);
+
+    Ok(())
+}
+
+#[command]
+pub fn cookie(ctx: &mut Context, msg: &Message) -> CommandResult {
+
+    let hypixel_token = env::var("HYPIXEL_TOKEN")
+        .expect("Expected hypixel token in the environment");
+
+    let skyblock_bazaar_cookie = format!("https://api.hypixel.net/skyblock/bazaar?key={}", hypixel_token);
+    let response = requests::get(skyblock_bazaar_cookie).unwrap();
+    let data = response.json().unwrap();
+
+    let buy_cookie_price = &data["products"]["BOOSTER_COOKIE"]["sell_summary"][0]["pricePerUnit"];
+
+    let _ = msg.channel_id.say(&ctx.http, buy_cookie_price);
 
     Ok(())
 }
