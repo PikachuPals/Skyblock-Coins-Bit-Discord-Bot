472acb66513399ccb92d89f2d9e43b3d4c3ce23c
Andy Lau <pikachupalsmail@gmail.com>
2020-10-21 12:57:42 +0100

    Added Dark Auction Support

--- src/commands/math.rs
+++ src/commands/math.rs
@@ -10,11 +10,11 @@ use serenity::utils::Colour;
 use requests::ToJson;
 
 use std::collections::HashMap;
-use std::env;
 use std::time::Instant;
-
-use std::sync::{Arc, Mutex, RwLock};
+use std::sync::mpsc;
 use std::thread;
+use std::sync::mpsc::Sender;
+use std::sync::{Arc, RwLock};
 
 #[command]
 pub fn multiply(ctx: &mut Context, msg: &Message, mut args: Args) -> CommandResult {
@@ -29,184 +29,206 @@ pub fn multiply(ctx: &mut Context, msg: &Message, mut args: Args) -> CommandResu
 }
 
 #[command]
-pub fn cookie(ctx: &mut Context, msg: &Message) -> CommandResult {
+pub fn da(ctx: &mut Context, msg: &Message) -> CommandResult {
 
-    let hypixel_token = env::var("HYPIXEL_TOKEN")
-        .expect("Expected hypixel token in the environment");
+  let start = Instant::now();
 
-    let start = Instant::now();
+  let _ = msg.channel_id.say(&ctx.http, "Working...");
 
-    let _ = msg.channel_id.say(&ctx.http, "Working...");
+//    let mojang_response = requests::get("https://api.mojang.com/users/profiles/minecraft/PikachuPals").unwrap();
+//    let mojang_data = mojang_response.json().unwrap();
+//    let user_uuid = mojang_data["id"].as_str().unwrap();
 
-    let skyblock_bazaar_cookie = format!("https://api.hypixel.net/skyblock/bazaar?key={}", hypixel_token);
-    let response = requests::get(skyblock_bazaar_cookie).unwrap();
-    let data = response.json().unwrap();
+//    let mut skyblock_request = String::from("https://api.hypixel.net/Skyblock/profiles?key=...&uuid=");
+//    skyblock_request.push_str(&user_uuid);
 
-    let buy_cookie_price = &data["products"]["BOOSTER_COOKIE"]["sell_summary"][0]["pricePerUnit"].as_f32().unwrap();
-    let default_bits: f32 = 4800.0;
-    let default_coins_per_bit = (buy_cookie_price/ default_bits).abs();
+//    let response = requests::get(skyblock_request).unwrap();
+//    let data = response.json().unwrap();
 
-    //    let mojang_response = requests::get("https://api.mojang.com/users/profiles/minecraft/PikachuPals").unwrap();
-    //    let mojang_data = mojang_response.json().unwrap();
-    //    let user_uuid = mojang_data["id"].as_str().unwrap();
+  let skyblock_auctions = String::from("https://api.hypixel.net/skyblock/auctions?page=0");
+  let response = requests::get(skyblock_auctions).unwrap();
+  let data = response.json().unwrap();
 
-    //    let mut skyblock_request = String::from("https://api.hypixel.net/Skyblock/profiles?key=...&uuid=");
-    //    skyblock_request.push_str(&user_uuid);
+  let auction_pages = data["totalPages"].as_i32().unwrap();
 
-    //    let response = requests::get(skyblock_request).unwrap();
-    //    let data = response.json().unwrap();
+  let categories: [String; 4] = ["Artifacts".to_string(), "Books".to_string(),"Pets".to_string(), "Travel Scrolls".to_string()];
+  let categories_count: [i32; 4] = [2, 5, 6, 1];
 
-    let skyblock_auctions = String::from("https://api.hypixel.net/skyblock/auctions?page=0");
-    let response = requests::get(skyblock_auctions).unwrap();
-    let data = response.json().unwrap();
+  let items: [String; 14] = ["Ender Artifact".to_string(), "Wither Artifact".to_string(),
+  "Sharpness VI".to_string(), "Giant Killer VI".to_string(), "Power VI".to_string(), "Growth VI".to_string(), "Protection VI".to_string(),
+  "Parrot Legendary".to_string(), "Parrot Epic".to_string(), "Turtle Legendary".to_string(), "Turtle Epic".to_string(), "Jellyfish Legendary".to_string(), "Jellyfish Epic".to_string(),
+  "Travel Scroll to Dark Auction".to_string()];
 
-    let auction_pages = data["totalPages"].as_i32().unwrap();
+  let dark_auction_lowest_prices = get_lowest_bin_values(auction_pages);
 
-    let bits_items_lowest_prices = get_lowest_bin_values(auction_pages);
+  let mut item_prices: HashMap<String, String> = HashMap::new();
 
-    let bits_item_cost_vec = vec![2000, 500, 3000, 300, 8000, 1200, 4000, 1500, 2000, 4000];
-    let item_array: [String; 10] = ["God Potion".to_string(), "Kat Flower".to_string(), "Heat Core".to_string(), "Hyper Catalyst Upgrade".to_string(), "Ultimate Carrot Candy Upgrade".to_string(),
-    "Colossal Experience Bottle Upgrade".to_string(), "Jumbo Backpack Upgrade".to_string(), "Minion Storage X-pender".to_string(), "Hologram".to_string(), "Enchanted Book".to_string()];
+  for item in items.iter(){
+      item_prices.insert(item.to_string() ,format!("$: {}", *dark_auction_lowest_prices.get(item).unwrap()));
+  }
 
-    let mut bits_item_vec = vec![];
+  let mut output_fields_vec = vec![];
 
-    for (item, price) in bits_items_lowest_prices.iter(){
-        let index = item_array.iter().position(|x| x == item).unwrap();
-        bits_item_vec.push(BitsItemPrices::new(item, bits_item_cost_vec[index], *price));
+  let mut count = 0;
+  let mut category = 0;
+
+  output_fields_vec.push((format!("`{}`", categories[0]),
+                        "ﾠﾠ",
+                        false,));
+
+for item in items.iter(){
+
+    if categories_count[category] > count {
+        count += 1;
+    }
+    else {
+
+        category += 1;
+        output_fields_vec.push((format!("ﾠﾠ\n`{}`", categories[category]),
+                                    "ﾠﾠ",
+                                    false,));
+        count = 1;
     }
 
-    let cookie_output = format!("*Booster Cookie Price:* `{}`\n*Current $/b:* `{:.1}`\nItems are organised into highest coins per bit.\nﾠﾠ", buy_cookie_price, default_coins_per_bit);
-
-    let mut output_fields_vec = vec![];
-
-    bits_item_vec.sort_by(|a, b| b.coins_per_bit().cmp(&a.coins_per_bit()));
-
-    for listing in bits_item_vec {
-        output_fields_vec.push((format!("{:.15}", listing.bits_item),
-                                format!("BIN: *{}*\n$/b: *{}*\nﾠﾠ", listing.lowest_cost, listing.coins_per_bit()),
-                                true,));
-    }
-
-    let timed_search = format!("Completed in {:.2?}.", start.elapsed());
-
-    let _msg = msg.channel_id.send_message(&ctx.http, |m|{
-        m.content(timed_search);
-        m.embed(|e| {
-            e.title("");
-            e.description(cookie_output);
-            e.thumbnail("https://i.imgur.com/JNpxJ7I.png");
-            e.colour(Colour::FOOYOO);
-            e.fields(output_fields_vec);
-            e });
-        m
-    });
-
-    Ok(())
-    }
-
-    #[derive(Hash, Eq, PartialEq, Debug)]
-    struct BitsItemPrices{
-    bits_item: String,
-    bits_cost: i32,
-    lowest_cost: i32,
-    }
-
-    impl BitsItemPrices{
-    fn new(bits_item: &str, bits_cost: i32, lowest_cost: i32) -> BitsItemPrices{
-        BitsItemPrices {bits_item: bits_item.to_string(), bits_cost: bits_cost, lowest_cost: lowest_cost}
-    }
-
-    fn coins_per_bit(&self) -> i32 {
-        (self.lowest_cost / self.bits_cost).abs()
-    }
-    }
-
-    fn get_lowest_bin_values(auction_pages: i32) ->  HashMap<String, i32>{
-
-    let item_array = ["God Potion".to_string(), "Kat Flower".to_string(), "Heat Core".to_string(), "Hyper Catalyst Upgrade".to_string(), "Ultimate Carrot Candy Upgrade".to_string(),
-    "Colossal Experience Bottle Upgrade".to_string(), "Jumbo Backpack Upgrade".to_string(), "Minion Storage X-pender".to_string(), "Hologram".to_string(), "Enchanted Book".to_string()];
-
-    let lowest_prices = Arc::new(RwLock::new(HashMap::new()));
-
-    for item in item_array.iter(){
-        let mut map = lowest_prices.write().unwrap();
-        map.insert(item.to_string(), Mutex::new(999999999));
-        drop(map);
-    }
-
-    let item_array = Arc::new(RwLock::new(item_array));
-
-    let mut handles = vec![];
-
-    for i in 0i32..=auction_pages{
-
-        let item_array = Arc::clone(&item_array);
-        let lowest_prices = Arc::clone(&lowest_prices);
-        let handle = thread::spawn(move || worker_thread(i, item_array, lowest_prices));
-        handles.push(handle);
-    }
-    for handle in handles{
-        handle.join().unwrap();
-    }
-
-    let mut return_hashmap: HashMap<String, i32> = HashMap::new();
-
-    let lowest_prices_output = &*lowest_prices.read().expect("RwLock poisoned.");
-
-    for (item, price) in lowest_prices_output {
-        let number = *price.lock().unwrap();
-        return_hashmap.insert(item.to_string(), number);
-    }
-
-    return return_hashmap;
+    output_fields_vec.push((format!("{}", item),
+    item_prices.get(item).unwrap(),
+    true,));
 }
 
-fn worker_thread(i: i32, item_array: Arc<RwLock<[String; 10]>>, lowest_prices: Arc<RwLock<HashMap<String, Mutex<i32>>>>) {
+  let timed_search = format!("Completed in {:.2?}.", start.elapsed());
 
-    let item_array_mutex = item_array.read().expect("RwLock poisoned.");
+  let _msg = msg.channel_id.send_message(&ctx.http, |m|{
+      m.content(timed_search);
+      m.embed(|e| {
+          e.title("Dark Auction");
+          e.description("\nﾠﾠ");
+          e.thumbnail("https://i.imgur.com/JNpxJ7I.png");
+          e.colour(Colour::FOOYOO);
+          e.fields(output_fields_vec);
+          e });
+      m
+  });
+  Ok(())
+}
 
-    let mut page_auctions = String::from(" https://api.hypixel.net/skyblock/auctions?page=");
-    let page_number = i.to_string();
-    page_auctions.push_str(&page_number);
+fn get_lowest_bin_values(auction_pages: i32) ->  HashMap<String, i32>{
 
-    let response = requests::get(page_auctions).unwrap();
-    let data = response.json().unwrap();
+  let item_array: [String; 14] = ["Ender Artifact".to_string(), "Wither Artifact".to_string(),
+  "Sharpness VI".to_string(), "Giant Killer VI".to_string(), "Power VI".to_string(), "Growth VI".to_string(), "Protection VI".to_string(),
+  "Parrot Legendary".to_string(), "Parrot Epic".to_string(), "Turtle Legendary".to_string(), "Turtle Epic".to_string(), "Jellyfish Legendary".to_string(), "Jellyfish Epic".to_string(),
+  "Travel Scroll to Dark Auction".to_string()];
 
-    let ebook = "Enchanted Book".to_string();
-    let expertise_enchant = "Expertise".to_string();
+  let locked_item_array = Arc::new(RwLock::new(item_array.clone()));
 
-    for auc in data["auctions"].members(){
+  let mut lowest_prices: HashMap<String, i32> = HashMap::new();
 
-        for auc_item in item_array_mutex.iter() {
+  for item in item_array.iter(){
+      lowest_prices.insert(item.to_string(), 999999999);
+  }
 
-            if auc["item_name"].as_str().unwrap() == auc_item && auc["bin"].as_bool() != None {
+  let mut sender_vector: Vec<Sender<i32>> = vec![];
+  let mut receiver_vector = vec![];
 
-                let low_price_mutex = lowest_prices.read().unwrap();
-                let lowest_price_of_item = low_price_mutex.get(auc_item).unwrap();
-                let mut number = *lowest_price_of_item.lock().unwrap();
-                drop(low_price_mutex);
-                let auc_item_price = auc["starting_bid"].as_i32().unwrap();
+  for _n in 0..item_array.len(){
+      let (tx, rx) = mpsc::channel();
+      sender_vector.push(tx);
+      receiver_vector.push(rx);
+  }
 
-                if auc_item == &ebook && auc["item_lore"].as_str().unwrap().contains(&expertise_enchant) {
-                    if auc_item_price < number {
-                        let mut low_price_write = lowest_prices.write().unwrap();
-                        number = auc_item_price;
-                        low_price_write.insert(auc_item.to_string(), Mutex::new(number));
-                        drop(low_price_write);
-                    }
-                }
+  let mut handles = vec![];
 
-                else if auc_item != &ebook {
+  let mut threads_pages: Vec<i32> = vec![0];
+  let threads: i32 = 15;
+  let pages_per_thread: i32 = auction_pages / threads;
+  let rem_pages: i32 = auction_pages % threads;
+  for thread in 1..=threads{
+      if thread != threads{
+          threads_pages.push(thread * pages_per_thread);
+      }
+      else{
+          threads_pages.push((thread * pages_per_thread) + rem_pages);
+      }
+  }
 
-                    if auc_item_price < number {
-                        let mut low_price_write = lowest_prices.write().unwrap();
-                        number = auc_item_price;
-                        low_price_write.insert(auc_item.to_string(), Mutex::new(number));
-                        drop(low_price_write);
-                    }
-                }
-                drop(number)
-            }
-        }
-    }
+  for i in 0..threads_pages.len() - 1 {
+      let mut sender_vector_clone: Vec<Sender<i32>> = vec![];
+      let locked_item_array_clone = locked_item_array.clone();
+      let start_page = threads_pages[i].clone();
+      let end_page = threads_pages[i + 1].clone();
+      for tx in &sender_vector{
+          let tx_clone = tx.clone();
+          sender_vector_clone.push(tx_clone);
+      }
+      let handle = thread::spawn(move || work_thread(sender_vector_clone, locked_item_array_clone,
+           start_page, end_page));
+      handles.push(handle);
+  }
+
+  for handle in handles{
+      handle.join().unwrap();
+  }
+
+  for sender in sender_vector{
+      drop(sender);
+  }
+
+  for item in item_array.iter() {
+      let index = item_array.iter().position(|x| x == item).unwrap();
+
+      for price in &receiver_vector[index]{
+          if price < *lowest_prices.get(item).unwrap() {
+              lowest_prices.insert(item.to_string(), price);
+          }
+      }
+  }
+
+  return lowest_prices;
+}
+
+fn work_thread(sender_vector: Vec<Sender<i32>>, locked_item_array: Arc<RwLock<[String; 14]>>, i: i32, e: i32){
+
+  let item_array = locked_item_array.read().unwrap();
+
+  for page in i..e{
+      let mut page_auctions = String::from(" https://api.hypixel.net/skyblock/auctions?page=");
+      let page_number = page.to_string();
+      page_auctions.push_str(&page_number);
+
+      let response = requests::get(page_auctions).unwrap();
+      let data = response.json().unwrap();
+
+      let ebook = "Enchanted Book".to_string();
+      let enchants: [String; 5] = ["Sharpness VI".to_string(), "Giant Killer VI".to_string(), "Power VI".to_string(), "Growth VI".to_string(), "Protection VI".to_string()];
+
+      for auc in data["auctions"].members(){
+          for auc_item in item_array.iter() {
+              if auc["bin"].as_bool() != None{
+                  if auc["item_name"].as_str().unwrap() == auc_item && auc["bin"].as_bool() != None {
+                      let index = item_array.iter().position(|x| x == auc_item).unwrap();
+                      let auc_item_price = auc["starting_bid"].as_i32().unwrap();
+                      sender_vector[index].send(auc_item_price).unwrap();
+                  }
+
+                  else if auc["item_name"].as_str().unwrap() == &ebook {
+                      for enchant in enchants.iter() {
+                          if auc["item_lore"].as_str().unwrap().contains(enchant){
+                              let index = item_array.iter().position(|x| x == enchant).unwrap();
+                              let auc_item_price = auc["starting_bid"].as_i32().unwrap();
+                              sender_vector[index].send(auc_item_price).unwrap();
+                          }
+                      }
+                  }
+
+                  else if auc_item.contains(&"Epic".to_string()) || auc_item.contains(&"Legendary".to_string()) {
+                      let mut pet = auc_item.split_whitespace();
+                      if auc["item_name"].as_str().unwrap().contains(pet.next().unwrap()) && auc["item_lore"].as_str().unwrap().contains(&pet.next().unwrap().to_uppercase()){
+                          let index = item_array.iter().position(|x| x == auc_item).unwrap();
+                          let auc_item_price = auc["starting_bid"].as_i32().unwrap();
+                          sender_vector[index].send(auc_item_price).unwrap();
+                      }
+                  }
+              }
+          }
+      }
+  }
 }
